51f426019fd5778567152572c1d766f6dad46780
Eli Mycaleb Mycaleb <>
2021-05-20 22:10:14 +0000

    Testing With Different codes

--- main.py
+++ main.py
@@ -7,4 +7,7 @@ btn=Button(window, text="Press To Continue", fg='Blue')
 btn.place(x=210, y=280)
 window.title('The Mental Health Quiz 2021')
 window.geometry("600x500+10+20")
+# Button for closing
+exit_button = Button(root, text="Exit", command=root.destroy) 
+
 window.mainloop()
