edd2a1d47fa4ed34aac730ea5f6b3aa6b338a8f2
Eli Mycaleb Mycaleb <>
2021-05-20 00:35:17 +0000

    Done part 1

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from tkinter import *
 window=Tk()
 # add widgets here
-btn=Button(window, text="Press To Continue", fg='Red') 
+lbl=Label(window, text="The Mental Health Quiz", fg='Black', font=("Serif", 21))
+lbl.place(x=110, y=50)
+btn=Button(window, text="Press To Continue", fg='Blue') 
 btn.place(x=210, y=280)
 window.title('The Mental Health Quiz 2021')
 window.geometry("600x500+10+20")
