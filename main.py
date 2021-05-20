321bb49a488abfed186284d610801a3f44ad71e2
Eli Mycaleb Mycaleb <>
2021-05-20 00:30:12 +0000

    Added a button

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from tkinter import *
 window=Tk()
 # add widgets here
-
+btn=Button(window, text="Press To Continue", fg='Red') 
+btn.place(x=210, y=280)
 window.title('The Mental Health Quiz 2021')
 window.geometry("600x500+10+20")
 window.mainloop()
